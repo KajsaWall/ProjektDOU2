8fc467ede4d427ed651d1932ec9d635311f67918
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-01-04 18:44:35 +0100

    function code

--- js/functions.js
+++ js/functions.js
@@ -34,6 +34,18 @@ function create_filter_element (data) {
       Returns a reference to the new dom-element
   */
 
+  const text = data.textContent;
+  const parent = data.parent;
+  const klass = data.class; 
+
+  const new_element = document.createElement("li");
+  new_element.classList.add(klass);
+  parent.appendChild(new_element);
+  new_element.textContent = text;
+  new_element.addEventListener("click", click_filter_element);
+
+  return new_element;
+
 }
 
 
